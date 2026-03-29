d0e375b8fd5c7d77a7f4f6bac1325d3cba5eea9c
Ankit Agarwal <121163166+Ankit-Agarwal10404@users.noreply.github.com>
2026-03-29 12:40:34 +0530

    Update CarRentSystem.java

--- CarRentSystem.java
+++ CarRentSystem.java
@@ -2,25 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
-class Customer {
-    private String customerId;
-    private String name;
-
-    public Customer(String customerId, String name) {
-        this.customerId = customerId;
-        this.name = name;
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public String getName() {
-        return name;
-    }
-}
-
 class Rental {
     private Car car;
     private Customer customer;
